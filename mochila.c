9504e13fb94761db5a9092ef40ff7885dc235175
Arthur Taylor <71983159+Eruel6@users.noreply.github.com>
2023-07-10 11:26:49 -0300

    update mochila
    Co-authored-by: itallogravina <itallo.gravina@gmail.com>
    
    melhora algoritmo para receber dados de um arquivo csv.

--- mochila.c
+++ mochila.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct 
+{
+    char nomeItem[100];
+    int peso;
+    int valor;
+} Item;
+
 
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-void knapsack(int pesos[], int valores[], int n, int capacidade) {
+void knapsack(Item itens[], int quantidadeItens, int capacidade) {
     int i, w;
-    int memoization[n + 1][capacidade + 1];
+    int memoization[quantidadeItens + 1][capacidade + 1];
 
-    for (i = 0; i <= n; i++) {
+    for (i = 0; i <= quantidadeItens; i++) {
         for (w = 0; w <= capacidade; w++) {
             if (i == 0 || w == 0) {
                 memoization[i][w] = 0;
-            } else if (pesos[i - 1] <= w) {
-                memoization[i][w] = max(valores[i - 1] + memoization[i - 1][w - pesos[i - 1]], memoization[i - 1][w]);
+            } else if (itens[i - 1].peso <= w) {
+                memoization[i][w] = max(itens[i - 1].valor + memoization[i - 1][w - itens[i - 1].peso], memoization[i - 1][w]);
             } else {
                 memoization[i][w] = memoization[i - 1][w];
             }
         }
     }
 
-    printf("Valor máximo: %d\n", memoization[n][capacidade]);
-    printf("Itens selecionados: ");
+    printf("Valor máximo: %d\n", memoization[quantidadeItens][capacidade]);
+    printf("Itens selecionados: \n");
 
     w = capacidade;
-    for (i = n; i > 0 && memoization[i][w] != 0; i--) {
+    for (i = quantidadeItens; i > 0 && memoization[i][w] != 0; i--) {
         if (memoization[i][w] != memoization[i - 1][w]) {
-            printf("%d ", i - 1);
-            w -= pesos[i - 1];
+            printf("%s \n", itens[i - 1].nomeItem);
+            w -= itens[i - 1].peso;
         }
     }
 
@@ -36,12 +45,39 @@ void knapsack(int pesos[], int valores[], int n, int capacidade) {
 }
 
 int main() {
-    int pesos[] = {2, 3, 4, 5};
-    int valores[] = {3, 4, 5, 6};
-    int capacidade = 7;
-    int n = sizeof(pesos) / sizeof(pesos[0]);
 
-    knapsack(pesos, valores, n, capacidade);
+    FILE *inputFile;
+    char inputFilename[] = "itens.csv";
+
+    Item itens[100];
+    char line[100];
+    int quantidadeItens = 0;
+
+    inputFile = fopen(inputFilename, "r");
+    if (inputFile == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 1;
+    }
+
+    // Lê os dados do arquivo de entrada
+    while (fgets(line, sizeof(line), inputFile)) {
+        char *token = strtok(line, ",");
+        strncpy(itens[quantidadeItens].nomeItem, token, sizeof(itens[quantidadeItens].nomeItem));
+        token = strtok(NULL, ",");
+        itens[quantidadeItens].peso = atoi(token);
+        token = strtok(NULL, ",");
+        itens[quantidadeItens].valor = atoi(token);
+        quantidadeItens++;
+    }
+
+    fclose(inputFile);
+
+    int peso[] = {2, 3, 4, 5};
+    int valor[] = {3, 4, 5, 6};
+    int capacidade = 30;
+    int n = sizeof(peso) / sizeof(peso[0]);
+
+    knapsack(itens, quantidadeItens, capacidade);
 
     return 0;
 }
